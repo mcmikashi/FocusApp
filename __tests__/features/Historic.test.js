ded8591515312ce9eed307d43234c1fa4a2dde6b
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-11-23 10:07:18 -0300

    Add historic feature that allow the user to show all previous focus subject

---
+++ __tests__/features/Historic.test.js
@@ -0,0 +1,57 @@
+import { render, screen, fireEvent } from "@testing-library/react-native";
+
+import Historic from "../../src/features/Historic";
+const focuslist  = [
+    {id:'1', title:'focus subject 1', status:false},
+    {id:'2', title:'focus subject 2', status:true},
+]
+test("check that correcly rendered when focuslist is empty", () => {
+    render(
+      <Historic
+        focusList={[]}
+      />
+    );
+    expect(screen.getByText("Nothing yet")).toBeTruthy();
+  
+});
+
+test("check that the focust list is rendered wiht not empty list", () => {
+  render(
+    <Historic
+      focusList={focuslist}
+    />
+  );
+  const focusSubjectOne = screen.getByText(focuslist[0].title)
+  expect(focusSubjectOne).toBeTruthy();
+  expect(focusSubjectOne.props.style.color).toEqual('red');
+  const focusSubjectTwo = screen.getByText(focuslist[1].title)
+  expect(focusSubjectTwo).toBeTruthy();
+  expect(focusSubjectTwo.props.style.color).toEqual('green');
+
+});
+
+test("check when clear button is pressed then clear function is called", () => {
+  const clearHistorymock = jest.fn();
+  render(
+    <Historic
+      focusList={focuslist}
+      clearHistory={clearHistorymock}
+    />
+  );
+  clearButton = screen.getByText("clear")
+  expect(clearButton).toBeTruthy()
+  fireEvent.press(clearButton);
+  expect(clearHistorymock).toBeCalled();
+});
+
+test("check that on press a subject then the focusSubject function is called", () => {
+    const focusSubjectmock = jest.fn();
+    render(
+      <Historic
+        focusList={focuslist}
+        focusSubject={focusSubjectmock}
+      />
+    );
+    fireEvent.press(screen.getByText(focuslist[0].title))
+    expect(focusSubjectmock).toBeCalledWith(focuslist[0]);
+});
---
+++ src/features/Historic.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, FlatList, Button, SafeAreaView} from 'react-native'
+import React from 'react'
+
+const Historic = ({focusList, clearHistory, focusSubject}) => {
+    const Item = ({ title, status, onPress }) => (
+        <View>
+          <Text style={styles.item(status)} onPress={onPress}>{title}</Text>
+        </View>
+    );
+    const renderItem = ({ item }) => (
+        <Item  title={item.title} status={item.status} onPress={() => focusSubject(item)}/>
+    );
+    
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things you havent focus on :</Text>
+        {focusList.length 
+        ? <View>
+                <SafeAreaView>
+                    <FlatList
+                        data={focusList}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                    />
+                </SafeAreaView>
+                <Button title='clear' onPress={() => clearHistory()}/>
+            </View>
+
+      
+        : <Text style={styles.simpleText}>Nothing yet</Text>
+        }
+    </View>
+  )
+}
+
+export default Historic
+
+const styles = StyleSheet.create({
+    container:{
+        marginVertical:15,
+    },
+    title:{
+        fontSize:18,
+        fontWeight:'800',
+        textAlign:'center',
+        marginVertical:15,
+    },
+    item:(status)=> ({
+        textAlign:'center',
+        color: status ? 'green' : 'red',
+        fontSize:16,
+        fontWeight:'600',
+        marginVertical:5
+    }),
+    simpleText:{
+        textAlign:'center'
+    }
+})
