9f562b501e1071aa2b0ebac5f0a2410493aedf46
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-11-23 10:02:00 -0300

    Add timing feature that allow the set timer of then counterown

---
+++ src/features/Timing.js
@@ -0,0 +1,26 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import RoundedButton from '../components/RoundedButton'
+const Timing = ({onChangeTimer}) => {
+    let timingButton = []
+    for (let index = 1; index < 5; index++) {
+        const valueTime = index * 5
+        timingButton.push(
+            <RoundedButton key={valueTime} title={valueTime.toString()} size={65} onPress={() => onChangeTimer(valueTime)}/>
+        )
+    }
+  return (
+    <View style={styles.container}>
+        {timingButton}
+    </View>
+  )
+}
+
+export default Timing
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    }
+})
